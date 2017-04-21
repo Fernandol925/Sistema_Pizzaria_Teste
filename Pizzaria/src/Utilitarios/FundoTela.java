6f158c21a33250b7a51e17fd3debf2973204fff6
Fernando Silva <fernandofloresdasilva@hotmail.com>
2017-04-20 21:01:17 -0300

    Arquivos

---
+++ Pizzaria/src/Utilitarios/FundoTela.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import java.awt.Image;
+import javax.swing.JDesktopPane;
+
+/**
+ *
+ * @author Fernando Silva
+ */
+public class FundoTela extends JDesktopPane {
+    private Image imagem;
+}
